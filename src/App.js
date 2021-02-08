76e4a260a82073968b017cf07de2e8df820ee5de
CGA <59731043+mohanvej@users.noreply.github.com>
2021-02-08 14:51:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 style={{ color: "red" }}>Hello CodeSandbox</h1>
+      <p
+        style={{
+          backgroundColor: "gray",
+          color: "white",
+          fontFamily: "verdana",
+          padding: "10px",
+          textAlign: "left"
+        }}
+      >
+        The quick brown fox jumps over the lazy dog The quick brown fox jumps
+        over the lazy dog The quick brown fox jumps over the lazy dog The quick
+        brown fox jumps over the lazy dog The quick brown fox jumps over the
+        lazy dog
+      </p>
+    </div>
+  );
+}
